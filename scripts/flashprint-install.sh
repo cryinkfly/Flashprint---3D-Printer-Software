05fe9de115cec13413fe08a3260c9ac0a1b90eb3
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-07-03 13:44:34 +0200

    Update flashprint-install.sh

--- scripts/flashprint-install.sh
+++ scripts/flashprint-install.sh
@@ -23,7 +23,13 @@ cd flashprint &&
 wget https://en.fss.flashforge.com/10000/software/eb49bb17d0d578dbd22b01e388c53579.deb &&
 ar -x eb49bb17d0d578dbd22b01e388c53579.deb &&
 tar -xf data.tar.xz &&
-su -c 'cp /etc/udev/rules.d/99-flashforge.rules $ROOT/etc/udev/rules.d/ && cp /usr/share/applications/FlashPrint.desktop $ROOT/usr/share/applications/ && cp -r /usr/share/doc/flashprint/ $ROOT/usr/share/doc/ && cp -r /usr/share/FlashPrint/ $ROOT/usr/share/ && cp /usr/share/icons/hicolor/64x64/apps/flashforge.png $ROOT/usr/share/icons/hicolor/64x64/apps/'
+su -c 'cp /etc/udev/rules.d/99-flashforge.rules $ROOT/etc/udev/rules.d/' &&
+su -c 'cp /usr/share/applications/FlashPrint.desktop $ROOT/usr/share/applications/' &&
+su -c 'cp -r /usr/share/doc/flashprint/ $ROOT/usr/share/doc/' &&
+su -c 'cp -r /usr/share/FlashPrint/ $ROOT/usr/share/' &&
+su -c 'cp /usr/share/icons/hicolor/64x64/apps/flashforge.png $ROOT/usr/share/icons/hicolor/64x64/apps/'
+
+
 echo "The installation of Flashprint is completed!"
 
 echo "Now You must restart your computer to make all changes effective!"
